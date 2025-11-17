5ab026f47194b93a9381efe05834900ad6e3cf95
Milad Fahham <milad.fahham@struct.dk>
2025-11-17 15:16:28 +0100

    Fixed some self-introduced explorer issues

--- Delivery.QueueWorker/QueueWorker.cs
+++ Delivery.QueueWorker/QueueWorker.cs
@@ -1,45 +1,13 @@
-﻿using Delivery.Application.Interfaces.Repositories;
-using Delivery.Application.Services;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Delivery.QueueWorker
 {
     public class QueueWorker
     {
-        IProductWriteRepository _productWriteRepository;
-        IProductReadRepository _productReadRepository;
-        IQueueReadRepository _queueReadRepository;
-        FilterDirtyIdsService _filterDirtyIdsService;
-
-        public QueueWorker(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IQueueReadRepository queueReadRepository, FilterDirtyIdsService filterDirtyIdsService)
-        {
-            _productWriteRepository = productWriteRepository;
-            _productReadRepository = productReadRepository;
-            _queueReadRepository = queueReadRepository;
-            _filterDirtyIdsService = filterDirtyIdsService;
-        }
-
-        public async Task ProcessQueueAsync()
-        {
-            var queuedItems = await _queueReadRepository.GetProductChanges(100);
-
-            if (!queuedChanges.Any())
-            {
-                return;
-            }
-
-            IEnumerable<string> cleanIds = await _filterDirtyIdsService.FilterDirtyIds(queuedChanges);
-
-            if (!cleanIds.Any())
-            {
-                return;
-            }
-
-            var data = await _productReadRepository.GetPimData(cleanIds);
-
-            await _productWriteRepository.CacheUpdates(data);
-
-            await _queueReadRepository.RemoveFromQueueAsync(cleanIds);
-
-        }
+        //public async Task ProcessQueueAsync() { } TO BE IMPLEMENTED
     }
 }
