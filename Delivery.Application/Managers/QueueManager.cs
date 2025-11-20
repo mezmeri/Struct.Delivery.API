5cafc84df990281ab9d09db3d214a0293ed59fc0
MagnusBroendum <mbsc58418@edu.ucl.dk>
2025-11-20 23:58:48 +0100

    Removed old comments in QueueManager, sorry bois

--- Delivery.Application/Managers/QueueManager.cs
+++ Delivery.Application/Managers/QueueManager.cs
@@ -13,63 +13,14 @@ namespace Delivery.Infrastructure.Managers
     {
         private readonly IQueueWriteRepository _queueWriteRepository;
 
-        // MOVE TO Domain -> QueueWorker -> Product
-
-        //private readonly IQueueReadRepository _queueReadRepository;
-        //private readonly IProductWriteRepository _productWriteRepository;
-        //private readonly IProductReadRepository _productReadRepository;
-        //private readonly PimApiService _pimApiService;
-        //private readonly FilterDirtyIdsService _filterDirtyIdsService;
-
         public QueueManager(IQueueWriteRepository queueWriteRepository)
-            //IQueueReadRepository queueReadRepository,IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, FilterDirtyIdsService filterDirtyIdsService, PimApiService pimApiService) 
         {
-            _queueWriteRepository = queueWriteRepository;
-            
-            // MOVE TO Domain -> QueueWorker -> Product
-
-            //_queueReadRepository = queueReadRepository;
-            //_productWriteRepository = productWriteRepository;
-            //_productReadRepository = productReadRepository;
-            //_filterDirtyIdsService = filterDirtyIdsService;
-            //_pimApiService = pimApiService;
+            _queueWriteRepository = queueWriteRepository;  
         }
 
         public async Task EnqueueUpdatesAsync(IEnumerable<string> ids)
         {
-            await _queueWriteRepository.AddToQueueAsync(ids);
-
-            //await ProcessQueueAsync();
+            await _queueWriteRepository.AddToQueueAsync(ids);   
         }
-
-        //public async Task ProcessQueueAsync()
-        //{
-        //    IEnumerable<(string, long)> queuedChanges = await _queueReadRepository.GetProductChanges(100);
-
-        //    if (!queuedChanges.Any())
-        //    {
-        //        return;  
-        //    }
-
-        //    IEnumerable<string> cleanIds = await _filterDirtyIdsService.FilterDirtyIds(queuedChanges);
-
-        //    List<string> dirtyIds = queuedChanges.Select(x => x.Item1).Except(cleanIds).ToList();
-
-
-        //    if (dirtyIds.Any())
-        //    {
-        //        await _queueReadRepository.RequeueIdsAsync(dirtyIds);
-        //    }
-
-        //    if (cleanIds.Any())
-        //    {
-        //        var data = await _pimApiService.GetProductDataAsync(cleanIds);
-
-        //        await _productWriteRepository.CacheUpdates(data);
-
-        //        await _queueReadRepository.RemoveFromQueueAsync(cleanIds);
-        //    }
-
-        //}
     }
 }
