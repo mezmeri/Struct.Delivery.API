142c9388cb9f0742a463546286ad2d8e7a21a07f
MagnusBroendum <mbsc58418@edu.ucl.dk>
2025-11-27 21:55:19 +0100

    Removed shitty comments

--- Delivery.QueueWorker/QueueWorker.cs
+++ Delivery.QueueWorker/QueueWorker.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-// HUSK HUSK!!! Add "Project Reference v højreklik af "Delivery.QueueWorker"
 using Delivery.Application.Interfaces.Repositories;
 using Delivery.Application.Services;
 
